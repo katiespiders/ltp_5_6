5403e371f55f62449e8068253dc7bbb34a742768
Katie <kab761@gmail.com>
2014-09-03 21:27:31 -0700

    first commit

---
+++ bigger_and_better.rb
@@ -0,0 +1,3 @@
+puts "What's your favorite number?"
+favorite = gets.chomp.to_i
+puts "May I suggest #{favorite + 1}? It's bigger and better and it should be your favorite."
---
+++ greeting.rb
@@ -0,0 +1,8 @@
+puts "What's your first name? "
+first = gets.chomp
+puts "Middle?"
+middle = gets.chomp
+puts "Last?"
+last = gets.chomp
+
+puts "Hey there #{first} #{middle} #{last}."
